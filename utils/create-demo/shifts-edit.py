d8818233aedec5283d90aadcf1ef807788f731f5
Regulus Berdin <rberdin@eztechsoft.com>
2025-06-05 11:42:22 +0800

    upd-script

--- utils/create-demo/shifts-edit.py
+++ utils/create-demo/shifts-edit.py
@@ -24,26 +24,37 @@ def get_odoo_connection(settings):
 
 SETTINGS = {
     'quest-staging': {
-        'dbname': "quest-dental-stage-20895900",
-        'host': "quest-dental-stage-20895900.dev.odoo.com",
+        'dbname': "quest-dental-stage2-20995329",
+        'host': "quest-dental-stage2-20995329.dev.odoo.com",
         'port': 443,
         'protocol': 'jsonrpc+ssl',
         'user': 'qdmcit@questdental.com.ph',
-        'admin_pwd': "12345",
+        'admin_pwd': "5adf5704705b0d7401c08f99b61df590cf81e0c7",
+    },
+    'quest-prod': {
+        'dbname': "quest-dental-prod-19092293",
+        'host': "quest-dental.odoo.com",
+        'port': 443,
+        'protocol': 'jsonrpc+ssl',
+        'user': 'qdmcit@questdental.com.ph',
+        'admin_pwd': "5adf5704705b0d7401c08f99b61df590cf81e0c7",
     },
 }
 
 
 if __name__ == "__main__":
-    setting = SETTINGS['quest-staging']
+    # setting = SETTINGS['quest-staging']
+    setting = SETTINGS['quest-prod']
     odoo = get_odoo_connection(setting)
     Shift = odoo.env['ez.shift']
 
     ids = Shift.search([])
+    pprint(setting)
     print(ids)
 
-    Shift.write(ids, {
-        'rounding_regular_work_minutes': 30,
-        'rounding_ot_minutes': 30,
-        'early_overtime': True,
-    })
+    if 0:
+        Shift.write(ids, {
+            'rounding_regular_work_minutes': 30,
+            'rounding_ot_minutes': 30,
+            'early_overtime': True,
+        })
